89cada18b6e85ba37d03fbffe865acdeab099a8d
William <xwilliamy@gmail.com>
2014-11-08 12:49:04 -0500

    Updated SuperArray.java

--- 01SuperArray/Driver.java
+++ 01SuperArray/Driver.java
@@ -1,9 +1,38 @@
 public class Driver{
     public static void main(String[]args){
-	SuperArray A = new SuperArray(1);
-	Integer x = new Integer(5);
-	A.add(5);
-	System.out.println(A.toString());
+	SuperArray A = new SuperArray();
+	Integer x = new Integer(1);
+       	A.add(x);
+	A.add(new Integer(2));
+	A.add(new Integer(3));
+	A.add(new Integer(4));
+	A.add(new Integer(5));
+	A.add(new Integer(6));
+	A.add("Seven");
+	A.add(new Integer(8));
+	A.add("Nine");
+	A.add("Ten");
 	System.out.println(A.size());
+	System.out.println(A.toString());
+	A.clear();
+	System.out.println(A.size());
+	System.out.println(A.toString());
+	A.add("One");
+	System.out.println(A.size());
+	System.out.println(A.toString());
+	A.add("Two");
+	A.add("three");
+	A.add("four");
+	A.resize(2);
+	System.out.println(A.toString());
+	A.resize(10);
+	A.add("four");
+	A.add("five");
+	System.out.println(A.toString());
+	System.out.println(A.get(2));
+	System.out.println(A.get(5));
+	A.set(0, "This is five");
+	A.set(5, "This is six");
+	System.out.println(A.toString());
     }
 }
--- 01SuperArray/SuperArray.java
+++ 01SuperArray/SuperArray.java
@@ -3,38 +3,70 @@ public class SuperArray{
     private int cNe, capacity; // current number of elements 
 
     public SuperArray(){
-	Object[] data = new Object[10];
-	cNe = 0;
+	data = new Object[10]; // max capacity ten
+	cNe = 0; // it has 0 elements so far
 	capacity = 10;
     }
 
     public SuperArray(int capacity){
-	Object[] data = new Object[capacity];
-	cNe = 0;
+	data = new Object[capacity]; // max capacity @ capacity
+	cNe = 0; // no elements
 	this.capacity = capacity;
     }
 
     public String toString(){
 	String answer = "[";
-	for(int i = 0; i < cNe;i++){
-	    answer += " " + data[i];
-	}
-	answer += "]";
-	return answer;
+	for(int i = 0; i < cNe;i++){ // stops once we added all elements
+	   answer += " " + data[i]+"; ";
+	 }
+	 answer += "]";
+	 return answer;
     }
 
     public void add(Object e){
-	if(cNe < capacity){
-	    data + e;
-	    cNe += 1; 
+	if(cNe < capacity){ //you can put in things as long as you don't exceed
+	    data[cNe] = e;
+	    cNe += 1;
 	}
 	else{
-	    System.out.println("You've entered too many.");
-	    }
+	    System.out.println("You have exceeded the SuperArray's size.");
+	}
     }
 
     public int size(){
 	return cNe;
+    }
+
+    public void resize(int newCapacity){
+	Object[] oldData = data;// copy all values 
+	data = new Object[newCapacity]; // make new one with new capacity
+	if(newCapacity < cNe){
+	    cNe = newCapacity; //change only if the new size is smaller
+	}
+	for(int i = 0; i< cNe; i++){
+	    //move from oldData to new data
+	    data[i] = oldData[i];
+	}
+    }
+
+    public void clear(){
+	data = new Object[capacity];
+	cNe = 0;
+    }
+
+    public Object get(int index){
+        return data[index];
+    }
+
+    public void set(int index, Object e){
+	if(index < cNe){
+	    data[index] = e;
+	    System.out.println("Passes here");
+	}
+	else{
+	    data[cNe] = e;
+	    System.out.println("Passes here");
+	}
     }
 
 }
