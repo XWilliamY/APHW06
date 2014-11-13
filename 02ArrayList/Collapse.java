c0e9a1f53ee490ec02b2e2c25f4fafa9a92cc111
William <xwilliamy@gmail.com>
2014-11-12 20:30:37 -0500

    if(L.get(i) == L.get(i-1)){ not working as it should

--- 02ArrayList/Collapse.java
+++ 02ArrayList/Collapse.java
@@ -16,12 +16,12 @@ public class Collapse{
 	collapseDuplicates(list);
 	System.out.println(list.toString());
 	ArrayList<Integer> list2 = new ArrayList<Integer>();
-	list2.add(new Integer(0));
-	list2.add(new Integer(0));
-	list2.add(new Integer(1));
-	list2.add(new Integer(1));
-	list2.add(new Integer(2));
-	list2.add(new Integer(0));
+	list2.add(0);
+	list2.add(0);
+	list2.add(1);
+	list2.add(1);
+	list2.add(2);
+	list2.add(0);
 	System.out.println(list2.toString());
 	collapseDuplicates(list);
 	System.out.println(list2.toString());
@@ -32,6 +32,7 @@ public class Collapse{
 	for(int i = 1; i < length; i++){
 	    if(L.get(i) == L.get(i-1)){
 		L.remove(i-1);
+		System.out.println("It checks out!");
 	    }
 	}
      
