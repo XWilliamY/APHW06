935a93980dfc8f09bdffb05516e3344af0046c00
William <xwilliamy@gmail.com>
2014-11-23 17:44:49 -0500

    Falling a bit behind schedule: still need to write addWordDiagonal function and make it so that all eight directions can be utilized. Also need to be able to import words from a text file and add them to WordGrid.

--- 03wordsearch/read.java
+++ 03wordsearch/read.java
@@ -1,2 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class read{
 
+    public static void main(String[]args)throws FileNotFoundException{
+	File text = new File("readThis.txt");
+	Scanner scnr = new Scanner(text);
+	ArrayList<String> list = new ArrayList<String>();
+
+	int lineNumber = 1;
+	String answer = "";
+	while(scnr.hasNextLine()){ // while there is another line 
+	    String line = scnr.nextLine();
+	    for(int i = 0; i < line.length(); i++){
+		if(!line.substring(i, i+1).equals(" ")){
+		    answer += line.substring(i, i+1);
+		}
+		else{
+		    break;
+		}
+	    }
+	    list.add(answer);
+	    lineNumber ++;
+	    answer = "";
+	}
+	System.out.println(list.toString());
+    }
+ 
+}
