8a0da04076873d7dba706c51820bca76bb3dbba8
William <xwilliamy@gmail.com>
2014-11-29 14:21:03 -0500

    Everything up to date!

--- 03wordsearch/Driver.java
+++ 03wordsearch/Driver.java
@@ -26,6 +26,22 @@ public class Driver{
 		System.out.println(w.wordsInPuzzle());
 		System.out.println(w.toString());
 	    }
+	    else if(args.length == 4){
+		int rows = Integer.parseInt(args[0]);
+		int cols = Integer.parseInt(args[1]);
+		int seed = Integer.parseInt(args[2]);
+		int answer = Integer.parseInt(args[3]);
+		WordGrid2 w = new WordGrid2(rows, cols);
+		w.setSeed(seed);
+		if(answer == 1){
+		    w.loadWordsFromFile("readThis.txt", false);
+		}
+		else{
+		    w.loadWordsFromFile("readThis.txt", true);
+		}
+		System.out.println(w.wordsInPuzzle());
+		System.out.println(w.toString());
+	    }
 	}
 	else{
 	    System.out.println("Proper Usage is: java Driver rows cols randomSeed answers");
