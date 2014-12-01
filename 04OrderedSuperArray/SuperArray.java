a503fb1a675312c875ff181a6489a3b57ff9ee4c
XWilliamY <xwilliamy@gmail.com>
2014-12-01 11:54:38 -0500

    Copied SuperArray.java to this folder

---
+++ 04OrderedSuperArray/SuperArray.java
@@ -0,0 +1,142 @@
+public class SuperArray{
+    private Object[]data;
+    private Object[]oldData;
+    private int cNe, capacity; // current number of elements 
+
+    public SuperArray(){
+	data = new Object[10]; // max capacity ten
+	cNe = 0; // it has 0 elements so far
+	capacity = 10;
+    }
+
+    public SuperArray(int capacity){
+	data = new Object[capacity]; // max capacity @ capacity
+	cNe = 0; // no elements
+	this.capacity = capacity;
+    }
+
+    public String toString(){
+	String answer = "[";
+	for(int i = 0; i < cNe;i++){ // stops once we added all elements
+	   answer += " " + data[i]+"; ";
+	 }
+	 answer += "]";
+	 return answer;
+    }
+
+    public void add(Object e){
+	if(cNe < capacity){ //if we didn't meet the capacity yet
+	    data[cNe] = e; //add at 0, 1, 2, up to capacity - 1
+	    cNe += 1;
+	}
+	else{
+	    capacity += 1; //increase capacity 
+	    // need to make a new data array with one more space at the end
+	    oldData = new Object[capacity];
+	    for(int i = 0; i < cNe; i++){
+		oldData[i] = data[i]; //transfer all the data to new array
+	    }
+	    oldData[cNe] = e; //add the new element
+	    data = new Object[capacity]; //reset data array
+	    data = oldData; //transfer back to data
+	    cNe ++;
+	}
+    }
+
+    //overloaded add 
+    public void add(int index, Object o){
+	//if it's out of range, use the basic add function
+	if(index < 0 || index >= size()){
+	    add(o);
+	}
+	else{
+	    if(cNe + 1 <= capacity){
+		oldData = new Object[capacity]; //no need to increase size
+	    }
+	    else{
+		capacity ++;
+		oldData = new Object[capacity];
+	    }
+
+	//otherwise we'll inject it into the array at the index
+	    // increase capacity if you can't add another element
+	    for(int i = 0; i < index; i++){
+		oldData[i] = data[i];
+	    } 
+	    oldData[index] = o;
+	    cNe ++; 
+	    for(int i = index; i <= cNe - 2; i++){
+		oldData[i+1] = data[i]; 
+	    }
+	}
+	data = new Object[capacity];
+	data = oldData;
+    }
+
+
+    public Object remove(int index){
+	Object B;
+	if(index < 0  || index >= size()){
+	    throw new IndexOutOfBoundsException();
+	}
+	else{
+	    // remove that element, move everything to the left 
+	    oldData = new Object[capacity];
+	    for(int i = 0; i < index; i++){
+		oldData[i] = data[i]; // transfer from one to the other
+	    }
+	    //now we want to skip whatever's at the index
+	    cNe --;
+	    B = data[index];
+	    for(int i = index +1; i < cNe; i++){
+		oldData[i+1] = data[i];
+	    }
+	}
+	data = new Object[capacity];
+	data = oldData;
+	return B;
+    }
+
+    public int size(){
+	return cNe;
+    }
+
+    public void resize(int newCapacity){
+	oldData = new Object[newCapacity];// copy all values 
+	for(int i = 0; i < cNe; i++){
+	    oldData[i] = data[i];
+	}
+	data = new Object[newCapacity];
+	data = oldData;
+	capacity = newCapacity;
+    }
+
+    public void clear(){
+	data = new Object[capacity];
+	cNe = 0;
+    }
+
+    public Object get(int index){
+	if(index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
+	}
+	else{
+        return data[index];
+	}
+    }
+
+    //overloaded set 
+    public Object set(int index, Object o){
+	Object A;
+	if(index < 0  || index >= size()){
+	    throw new IndexOutOfBoundsException();
+	}
+	else{
+	    A = get(index);
+	    data[index] = o;
+	}
+	return A;
+    }
+
+    
+}
