85b3a32dd18bd80d9d67be735bd94ef973cd0a5a
William <xwilliamy@gmail.com>
2014-12-01 20:28:11 -0500

    Figuring out how add will work

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -11,25 +11,22 @@ public class OrderedSuperArray extends SuperArray{
     //if you add to the end/ beginning, swapping is all you need to do
 
     public void add(String e){
+	/**So here's how we're going to do add
+	 *First, we add it to the end. Swapping is better than inserting and making new arrays and what not.
+	 *After we add it to the list, we want to see where it fits. We'll check it with each element. Since we're putting it where it fits, it doesn't matter what the user input is, since we're always going to put it at the end first.
+	 *Anyways, we're going to run through each word to see where it belongs.
+	 *So let's say adding "banana" to "apple" and "cake"
+	 * b > a? yes. b > c? No. Perfect, it'll go here. 
+	 *apple, cake, banana -> apple, banana,c ake
+	 *What if it's equal?
+	 *We'll move on to the second letter, third, whatever, until we reach the end of the shorter word. If the words are the same but nested, like "apple" and "applesauce", the shorter word goes first. 
+	 *Let's get coding though.
+	 **/ 
+
 	super.add(e);
 	int cne = getcNe();
-	Object [] yes = getdata();
-	/** Otherwise, what we'll do is we'll add it to the end first
-	 *and then from there, we compare it to the elements already in the list
-	 *and swap the words around until we reach its correct destination
-	 */ 
-	for(int i = 0; i < cne; i++){
-	    if(yes[i].equals(e)){
-		//swap until it's after or before the other word
-		System.out.println("damn");
-	    }
-	    else{
-		System.out.println("jk");
-		//check if the letters start out the same 
-		//if so move it
-	    }
-	}
     }
+
 
 
 
