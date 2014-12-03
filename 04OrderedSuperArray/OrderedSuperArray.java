f6035784840fab174c4d5dd7a214cc50301c6888
William <xwilliamy@gmail.com>
2014-12-02 19:31:32 -0500

    add() functional for String

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -3,6 +3,11 @@ public class OrderedSuperArray extends SuperArray{
     public static void main(String[]args){
 	OrderedSuperArray A = new OrderedSuperArray();
 	System.out.println(A.toString());
+	A.add("what");
+	A.add("please");
+	A.add("hello");
+	A.add("A");
+	System.out.println(A.toString());
     }
 
     public void add(String e){
@@ -10,14 +15,15 @@ public class OrderedSuperArray extends SuperArray{
 	    super.add(e);
 	}
 	else{
+	    int wordBefore = 0;
 	    for(int i = 0; i < super.size(); i++){
 		if(super.get(i).compareTo(e) >= 0){
 		    super.add(i, e);
+		    break;
 		}
 	    }
 	}
     }
-
 
 
 }
