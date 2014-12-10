ab7578ac4a0f0d8c8c9cbab77eeb6a27d498f522
XWilliamY <xwilliamy@gmail.com>
2014-12-10 12:23:00 -0500

    Celsius and Fahrenheit buttons added

--- CelsiusFahrenheit/convert.java
+++ CelsiusFahrenheit/convert.java
@@ -4,27 +4,28 @@ import java.awt.*;
 
 public class convert extends JFrame{
     private Container pane;
-    private JButton Celsius, Fahrenheit, b;
+    private JButton Celsius, Fahrenheit;
     private JLabel l;
     private JTextField t;
-    private JCheckBox c;
     public convert(){
-	this.setTitle("My first GUI");
+	this.setTitle("Celsius-Fahrenheit Converter");
 	this.setSize(600, 100);
 	this.setLocation(100, 100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	pane = this.getContentPane();
 	pane.setLayout(new FlowLayout());
-	b = new JButton("Do Nothing");
+	Celsius = new JButton("Convert to C");
+	Fahrenheit = new JButton("Convert to F");
 	l = new JLabel("Convert a value to Celsius or Fahrenheit:", null, JLabel.CENTER);
 	t = new JTextField(12);
-	c = new JCheckBox("OVERCLOCK!!!");
 	pane.add(l);
-	pane.add(b);
+	pane.add(Celsius);
+	pane.add(Fahrenheit);
 	pane.add(t);
-	pane.add(c);
-
     }
+
+
+
 
     public static void main(String[]args){
 	convert g = new convert();
