23c5fff9299b4e4ae9df401b44245399f29de1f2
XWilliamY <xwilliamy@gmail.com>
2014-12-10 12:20:07 -0500

    updated jlabel

--- CelsiusFahrenheit/convert.java
+++ CelsiusFahrenheit/convert.java
@@ -1,11 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
+
 
 public class convert extends JFrame{
+    private Container pane;
+    private JButton Celsius, Fahrenheit, b;
+    private JLabel l;
+    private JTextField t;
+    private JCheckBox c;
     public convert(){
 	this.setTitle("My first GUI");
 	this.setSize(600, 100);
 	this.setLocation(100, 100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
+	b = new JButton("Do Nothing");
+	l = new JLabel("Convert a value to Celsius or Fahrenheit:", null, JLabel.CENTER);
+	t = new JTextField(12);
+	c = new JCheckBox("OVERCLOCK!!!");
+	pane.add(l);
+	pane.add(b);
+	pane.add(t);
+	pane.add(c);
+
     }
 
     public static void main(String[]args){
