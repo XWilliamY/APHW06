4f2c5d3006f6cf82ff364cf79489efcd71163d89
XWilliamY <xwilliamy@gmail.com>
2014-12-16 12:28:59 -0500

    I think bubblesort works

--- 06sorts/sorts.java
+++ 06sorts/sorts.java
@@ -7,12 +7,31 @@ public class sorts extends SuperArray{
 	//8, 7, 0, 1, 2, 3, 9
 	//7, 0, 1, 2, 3, 8, 9
 	//kind of linear search 
-	for(int i = 0; i < super.size(); i++){
-	    System.out.println("yes");
+	for(int i = 0; i < super.size()-1; i++){
+	    int originalO = super.get(i);
+	    int newO = super.get(i+1);
+	    int originalIndex = i;
+	    int newIndex = i+1;
+	    Boolean go = true;
+	    while(go){
+		if(originalO > newO){
+		    //as long as the original value is greater, keep swapping
+		    set(newIndex, originalO);
+		    set(originalIndex, newO);
+		    newIndex ++;
+		    newO = super.get(newIndex);
+		}
+		else{
+		    go = false;
+		}
+	    }
 	}
     }
 
     public static void main(String[]args){
-	SuperArray A = new SuperArray();
-	A.add("one");
-	A.add("two");
+	sorts A = new sorts();
+	A.add(1);
+	A.add(2);
+	A.bubbleSort();
+    }
+}
