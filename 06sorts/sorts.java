e830a87add73c3a1396c6ac2dda72db803b26de4
William <xwilliamy@gmail.com>
2014-12-16 21:43:55 -0500

    sorts.java now features selectionSort bubbleSort and insertionSort. Not quite sure how to make it generic though.

--- 06sorts/sorts.java
+++ 06sorts/sorts.java
@@ -1,37 +1,19 @@
 public class sorts extends SuperArray{
 
-    public void bubbleSort(){
-	//how bubbleSort works
-	//9, 8, 7, 0, 1, 2, 3
-	//9 > 8, swap, 9 > 7, swap, 9>0, swap, 9>1, swap...
-	//8, 7, 0, 1, 2, 3, 9
-	//7, 0, 1, 2, 3, 8, 9
-	//kind of linear search 
-	for(int i = 0; i < super.size()-1; i++){
-	    String originalO = super.get(i);
-	    String newO = super.get(i+1);
-	    int originalIndex = i;
-	    int newIndex = i+1;
-	    Boolean go = true;
-	    while(go){
-		if(originalO > newO){
-		    //as long as the original value is greater, keep swapping
-		    set(newIndex, originalO);
-		    set(originalIndex, newO);
-		    newIndex ++;
-		    newO = super.get(newIndex);
-		}
-		else{
-		    go = false;
-		}
-	    }
-	}
-    }
-
     public static void main(String[]args){
 	sorts A = new sorts();
-	A.add(1);
-	A.add(2);
-	A.bubbleSort();
+	A.add("yes");
+	A.add("no");
+	A.add("zebra");
+	A.add("elephant");
+	A.selectionSort();
+	System.out.println(A.toString());
+	sorts B = new sorts();
+	B.add("zombie");
+	B.add("elephant");
+	B.add("apple");
+	B.add("cancer");
+	B.bubbleSort();
+	System.out.println(B.toString());
     }
 }
